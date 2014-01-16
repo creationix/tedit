7a46ef6138185ac0ceed3bc3385a221c6bd81024
Tim Caswell <tim@creationix.com>
2014-01-16 11:57:23 -0800

    Import tree style from original tedit

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/javascript");
 
-console.log(require)
+console.log(ace.require)
---
+++ background.js
@@ -0,0 +1,6 @@
+/*global chrome*/
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('/index.html', {
+    id: "tedit",
+  });
+});
