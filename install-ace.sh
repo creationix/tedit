f4a8e9e86ae53fb0e00f112b37be1b44f28d9f73
Tim Caswell <tim@creationix.com>
2014-01-20 09:48:53 -0600

    Use upstream ace-builds to get workers working

--- install-ace.sh
+++ install-ace.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 cd `dirname $0`
-curl -L https://github.com/zefhemel/ace-builds/archive/master.tar.gz | tar xzf -
+curl -L https://github.com/ajaxorg/ace-builds/archive/master.tar.gz | tar xzf -
 rm -rf ace
-mv ace-builds-master/ace ace
+mv ace-builds-master/src-noconflict ace
+rm -rf ace/snippets
 rm -rf ace-builds-master
