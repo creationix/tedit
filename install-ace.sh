320e689fa0ee8b20e5c57dc4266a1d3f17b9779a
Tim Caswell <tim@creationix.com>
2014-02-07 04:44:03 +0000

    Restructure to get ready for self-hosting

--- install-ace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-cd `dirname $0`
-curl -L https://github.com/ajaxorg/ace-builds/archive/master.tar.gz | tar xzf -
-rm -rf ace
-mv ace-builds-master/src-min-noconflict ace
-rm -rf ace/snippets
-rm -rf ace-builds-master
