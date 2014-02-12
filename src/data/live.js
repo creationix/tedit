5cdf2f0071c1e795b42f48d3f2a3fb17770b4a13
Tim Caswell <tim@creationix.com>
2014-02-12 21:00:15 +0000

    Use trampoline to avoid stack overflow.

--- src/data/live.js
+++ src/data/live.js
@@ -32,9 +32,11 @@ define("data/live", function () {
 
     return hook;
 
-    function hook(node) {
+    function hook(newNode) {
+      node = newNode;
+      if (!rootEntry) return;
       // If it's busy doing an export, put the node in the dirty queue
-      if (node.pulse) {
+      if (queue) {
         dirty = node;
         return;
       }
@@ -42,90 +44,115 @@ define("data/live", function () {
 
       // Mark the process as busy
       node.pulse = true;
-      console.log("Export Start");
+      queue = [];
 
-      // Run the export script
-      exportPath(settings.source, settings.name, rootEntry, onExport, onFail);
-
-      function onExport() {
-        node.pulse = false;
-        notify("Finished Export");
-        console.log("Export Done");
-        // If there was a pending request, run it now.
-        if (dirty) {
-          var newNode = dirty;
-          dirty = null;
-          hook(newNode);
-        }
-      }
-
-      function onFail(err) {
-        node.pulse = false;
-        notify("Export Failed");
-        fail(node, err);
-      }
-
+      pending = 0;
+      enqueue(exportPath, settings.source, settings.name, rootEntry);
+      onSuccess();
     }
 
+    function enqueue(fn) {
+      var args = Array.prototype.slice.call(arguments, 1);
+      queue.push({fn:fn,args:args});
+    }
 
-    function exportPath(path, name, dir, onSuccess, onError) {
+    var queue, checking, pending, sync;
+    function onSuccess() {
+      if (checking) {
+        sync = true;
+        return;
+      }
+      checking = true;
+      while (queue.length) {
+        var next = queue.shift();
+        sync = false;
+        next.fn.apply(null, next.args);
+        if (!sync) break;
+      }
+      checking = false;
+      if (!pending) onDone();
+    }
+
+    function onDone() {
+      node.pulse = false;
+      queue = null;
+      notify("Finished Export to " + rootEntry.fullPath + "/" + settings.name);
+      // If there was a pending request, run it now.
+      if (dirty) {
+        var newNode = dirty;
+        dirty = null;
+        hook(newNode);
+      }
+    }
+
+    function onError(err) {
+      node.pulse = false;
+      notify("Export Failed");
+      fail(node, err);
+    }
+
+    function exportPath(path, name, dir) {
       var etag = memory[path];
+      pending++;
       return servePath(path, etag, onEntry);
 
       function onEntry(err, entry) {
+        pending--;
         if (!entry) return onError(err || new Error("Can't find " + path));
-        return exportEntry(path, name, dir, entry, onSuccess, onError);
+        return exportEntry(path, name, dir, entry);
       }
     }
 
-    function exportEntry(path, name, dir, entry, onSuccess, onError) {
+    function exportEntry(path, name, dir, entry) {
       var etag = memory[path];
       // Always walk trees because there might be symlinks under them that point
       // to changed content without  the tree's content actually changing.
-      if (entry.tree) return exportTree(path, name, dir, entry.tree, onSuccess, onError);
+      if (entry.tree) return exportTree(path, name, dir, entry.tree);
       // If the etags match, it means we've already exported this version of this path.
       if (etag && entry.etag === etag) {
         // console.log("Skipping", path, etag);
         return onSuccess();
       }
+      notify("Exporting to " + rootEntry.fullPath + "/" + path + "...");
       // console.log("Exporting", path, etag);
 
       // Mark this as being saved.
       memory[path] = entry.etag;
+      pending++;
       entry.fetch(onBody);
 
       function onBody(err, body) {
+        pending--;
         if (body === undefined) return onError(err || new Error("Problem fetching response body"));
-        return exportFile(name, dir, body, onSuccess, onError);
+        return exportFile(name, dir, body);
       }
     }
 
-    function exportTree(path, name, dir, tree, onSuccess, onError) {
+    function exportTree(path, name, dir, tree) {
       // Create the directoy
+      pending++;
       dir.getDirectory(name, {create: true}, onDir, onError);
 
       function onDir(dirEntry) {
+        pending--;
         // Export it's children.
-        exportChildren(path, tree, dirEntry, onSuccess, onError);
+        exportChildren(path, tree, dirEntry);
       }
     }
 
-    function exportChildren(base, tree, dir, onSuccess, onError) {
-      var names = Object.keys(tree);
-      check();
-
-      function check() {
-        var name = names.shift();
-        if (!name) return onSuccess();
-        exportPath(base + "/" + name, name, dir, check, onError);
-      }
+    function exportChildren(base, tree, dir) {
+      Object.keys(tree).forEach(function (name) {
+        enqueue(exportPath, base + "/" + name, name, dir);
+      });
+      onSuccess();
     }
 
-    function exportFile(name, dir, body, onSuccess, onError) {
+    function exportFile(name, dir, body) {
       // Flag for onWriteEnd to know state
       var truncated = false;
 
       // Create the file
+      pending++;
       dir.getFile(name, {create:true}, onFile, onError);
 
       // Create a writer for the file
@@ -142,7 +169,10 @@ define("data/live", function () {
 
       // This gets called twice.  The first calls truncate and then comes back.
       function onWriteEnd() {
-        if (truncated) return onSuccess();
+        if (truncated) {
+          pending--;
+          return onSuccess();
+        }
         truncated = true;
         // Trim any extra data leftover from a previous version of the file.
         this.truncate(this.position);
@@ -150,6 +180,7 @@ define("data/live", function () {
     }
 
     function handleFilter(req, callback) {
+      throw "ME"
       var handler = handlers[req.name];
       if (handler) return handler(servePath, req, callback);
       return callback(new Error("Unknown filter handler " + JSON.stringify(req.name)));
