b334ca25ff42dd4cafbe7699abecbb5e421de3ec
Tim Caswell <tim@creationix.com>
2014-02-16 06:46:25 +0000

    Implement basic tree open/close in tree-v6

--- src/ui/tree-6.js
+++ src/ui/tree-6.js
@@ -2,19 +2,70 @@ var rootEl = require('./elements').tree;
 var fs = require('data/fs');
 var makeRow = require('./row');
 var modes = require('js-git/lib/modes');
+var prefs = require('./prefs');
 
-console.log({
-  rootEl: rootEl,
-  fs: fs
-});
+// Memory for opened trees.  Accessed by path
+var openPaths = prefs.get("openPaths", {});
 
-// Basic check to know of nothing has changed in the root.
+// Basic check to know if nothing has changed in the root.
 var rootHash;
+// Rows indexed by path
+var rows = {};
 
-fs.addRoot("test", {githubName:"creationix/tedit-app"}, function (err) {
+fs.addRoot("test", {githubName:"creationix/tedit-app"}, function (err, name) {
   if (err) fail("", err);
+  openPaths[name] = true;
   fs.readTree("", onRoots);
 });
+
+rootEl.addEventListener("click", onGlobalClick, false);
+
+function nullify(evt) {
+  evt.preventDefault();
+  evt.stopPropagation();
+}
+
+function findRow(element) {
+  while (element !== rootEl) {
+    if (element.js) return element.js;
+    element = element.parentNode;
+  }
+}
+
+function onGlobalClick(evt) {
+  var row = findRow(evt.target);
+  if (!row) return;
+  nullify(evt);
+  if (row.mode === modes.tree || row.mode === modes.commit) {
+    if (openPaths[row.path]) closeTree(row);
+    else openTree(row);
+  }
+  else {
+    console.log("onClick", row);
+  }
+}
+
+function openTree(row) {
+  var path = row.path;
+  row.busy++;
+  row.open = true;
+  fs.readTree(path, onTree);
+
+  function onTree(err, tree) {
+    row.busy--;
+    if (!tree) fail(path, err || new Error("Missing tree " + path));
+    openPaths[path] = true;
+    prefs.save();
+    renderChildren(row, tree);
+  }
+}
+
+function closeTree(row) {
+  row.removeChildren();
+  row.open = false;
+  delete openPaths[row.path];
+  prefs.save();
+}
 
 function onRoots(err, tree, hash) {
   if (err) fail("", err);
@@ -28,15 +79,22 @@ function onRoots(err, tree, hash) {
   });
 }
 
-function renderChild(path, name, mode, hash, parent) {
+function renderChild(path, name, mode, hash) {
   // console.log("renderChild", arguments);
-  var row = makeRow(path, mode, hash);
+  var row = rows[path];
+  if (row) {
+    row.mode = mode;
+    if (row.hash === hash) return row;
+    row.hash = hash;
+  }
+  else {
+    row = rows[path] = makeRow(path, mode, hash);
+  }
   if (mode === modes.commit) {
     row.busy++;
     fs.readCommit(path, onCommit);
   }
-  // TODO: look up openPaths config instead of hard-coding to true
-  if ((mode === modes.tree) && true) {
+  if ((mode === modes.tree) && openPaths[path]) {
     row.busy++;
     fs.readTree(path, onTree);
   }
@@ -47,7 +105,7 @@ function renderChild(path, name, mode, hash, parent) {
     if (!commit) fail(path, err || new Error("Missing commit " + path));
     row.treeHash = commit.tree;
     // TODO: look up openPaths config instead of hard-coding to true
-    if (true) {
+    if (openPaths[path]) {
       row.busy++;
       fs.readTree(path, onTree);
     }
@@ -56,12 +114,19 @@ function renderChild(path, name, mode, hash, parent) {
   function onTree(err, tree) {
     row.busy--;
     if (!tree) fail(path, err || new Error("Missing tree " + path));
-    Object.keys(tree).forEach(function (name) {
-      var entry = tree[name];
-      var child = renderChild(path + "/" + name, name, entry.mode, entry.hash);
-      row.addChild(child);
-    });
+    renderChildren(row, tree);
   }
+}
+
+function renderChildren(row, tree) {
+  var path = row.path;
+  // TODO: this might be an update in which case we need to reuse as many
+  // children as possible.
+  Object.keys(tree).forEach(function (name) {
+    var entry = tree[name];
+    var child = renderChild(path + "/" + name, name, entry.mode, entry.hash);
+    row.addChild(child);
+  });
 }
 
 function renderCommit(path) {
