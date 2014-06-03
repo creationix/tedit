e9c9d394680d9360dbf77223000b280063415e9f
Tim Caswell <tim@creationix.com>
2014-06-03 05:25:46 +0000

    Add cjs module system to eval runtime.

--- src/runtimes/local-eval.js
+++ src/runtimes/local-eval.js
@@ -7,10 +7,81 @@ exports.menuItem = {
 
 var editor = require('ui/editor');
 var tree = require('ui/tree');
+var mine = require('mine');
+var fs = require('data/fs');
+var pathJoin = require('pathjoin');
+var bodec = require('bodec');
 
 function execFile(row) {
+
+  var modules = {};
+  var defs = {};
+  var loading = {};
+
   tree.activateDoc(row, true, function () {
     var js = editor.getText();
-    (new Function(js))();
+    var path = "vfs:" + row.path;
+    load(path, js, function (err) {
+      if (err) row.fail(err);
+      try { fakeRequire(path); }
+      catch (err) { row.fail(err); }
+    });
   });
+
+  function fakeRequire(path) {
+    if (path in modules) {
+      return modules[path].exports;
+    }
+    if (path in defs) {
+      var exports = {};
+      var module = modules[path] = { exports: exports };
+      defs[path](fakeRequire, module, exports, pathJoin(path, ".."), path);
+      return module.exports;
+    }
+    return require(path);
+  }
+
+  function load(path, js, callback, force) {
+    if (!force && (defs[path] || modules[path] || loading[path])) return callback();
+    loading[path] = true;
+    if (js === null || js === undefined) {
+      return fs.readBlob(path.substring(4), function (err, entry) {
+        if (err || !entry || !entry.hash) {
+          return callback(err || new Error("Missing file " + path));
+        }
+        return load(path, bodec.toUnicode(entry.blob), callback, true);
+      });
+    }
+    var left = 1;
+    mine(js).reverse().forEach(function (dep) {
+      var len = dep.name.length;
+      if (!/\.js$/.test(dep.name)) {
+        dep.name += ".js";
+      }
+      if (dep.name[0] === ".") {
+        dep.name = pathJoin(path, "..", dep.name);
+        left++;
+        load(dep.name, null, check);
+      }
+      js = js.substring(0, dep.offset) + dep.name + js.substring(dep.offset + len);
+    });
+    defs[path] = new Function("require", "module", "exports", "__dirname", "__filename", js);
+    check();
+    var done;
+
+    function check(err) {
+      if (done) return;
+      if (err) {
+        done = true;
+        return callback(err);
+      }
+
+      if (!--left) {
+        done = true;
+        return callback();
+      }
+    }
+
+  }
+
 }
