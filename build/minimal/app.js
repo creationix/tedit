9e3ff9e664073b7dc2b100a1aecbd8b2b2beea31
Tim Caswell <tim@creationix.com>
2014-06-26 22:41:07 -0500

    Speed test

--- build/minimal/app.js
+++ build/minimal/app.js
@@ -115,9 +115,9 @@ function AppWindow(emit, refresh) {
     } : {
       width: width + "px",
       height: height + "px",
-      // webkitTransform: "translate3d(" + left + "px," + top + "px,0)",
-      left: left + "px",
-      top: top + "px",
+      webkitTransform: "translate3d(" + left + "px," + top + "px,0)",
+      // left: left + "px",
+      // top: top + "px",
     };
     return ["dialog.window", {
         style: style, class: isDark ? "dark" : "light"
@@ -293,39 +293,40 @@ function AppWindow(emit, refresh) {
 }
 
 function CodeMirrorEditor() {
-  var code, mode, theme;
-  var el;
-  var cm = new CodeMirror(function (root) {
-    el = root;
-  }, {
-    keyMap: "sublime",
-    // lineNumbers: true,
-    rulers: [{ column: 80 }],
-    autoCloseBrackets: true,
-    matchBrackets: true,
-    showCursorWhenSelecting: true,
-    styleActiveLine: true,
-  });
-  setTimeout(function () {
-    cm.refresh();
-  }, 0);
+  // var code, mode, theme;
+  // var el;
+  // var cm = new CodeMirror(function (root) {
+  //   el = root;
+  // }, {
+  //   keyMap: "sublime",
+  //   // lineNumbers: true,
+  //   rulers: [{ column: 80 }],
+  //   autoCloseBrackets: true,
+  //   matchBrackets: true,
+  //   showCursorWhenSelecting: true,
+  //   styleActiveLine: true,
+  // });
+  // setTimeout(function () {
+  //   cm.refresh();
+  // }, 0);
 
   return { render: render };
 
   function render(isDark, props) {
-    var newTheme = isDark ? "notebook-dark" : "notebook";
-    if (newTheme !== theme) {
-      theme = newTheme;
-      cm.setOption("theme", theme);
-    }
-    if (props.mode !== mode) {
-      mode = props.mode;
-      cm.setOption("mode", mode);
-    }
-    if (props.code !== code) {
-      code = props.code;
-      cm.setValue(code);
-    }
+    // var newTheme = isDark ? "notebook-dark" : "notebook";
+    // if (newTheme !== theme) {
+    //   theme = newTheme;
+    //   cm.setOption("theme", theme);
+    // }
+    // if (props.mode !== mode) {
+    //   mode = props.mode;
+    //   cm.setOption("mode", mode);
+    // }
+    // if (props.code !== code) {
+    //   code = props.code;
+    //   cm.setValue(code);
+    // }
+    return [];
     return el;
   }
 }
