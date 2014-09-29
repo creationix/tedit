3db24c5dbfd80aa9af2d34de136f487d9cefd778
Tim Caswell <tim@creationix.com>
2014-09-29 13:03:26 -0500

    Use full path to bundler library

--- build/web/tedit.js
+++ build/web/tedit.js
@@ -1,3 +1,3 @@
 #!js
 
-module.exports = require('../../lib/wheaty-cjs-bundler')("src/main-web.js", ["src","lib"]);
+module.exports = require('../../lib/wheaty-cjs-bundler/bundler.js')("src/main-web.js", ["src","lib"]);
